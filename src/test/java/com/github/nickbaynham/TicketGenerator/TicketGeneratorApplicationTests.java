bcc0065eeaef3a5a8e03cf7e185548f17842e28a
Nick Baynham <nick.baynham@oracle.com>
2020-12-19 13:57:13 -0500

    added import for junit.Assert

--- src/test/java/com/github/nickbaynham/TicketGenerator/TicketGeneratorApplicationTests.java
+++ src/test/java/com/github/nickbaynham/TicketGenerator/TicketGeneratorApplicationTests.java
@@ -3,6 +3,8 @@ package com.github.nickbaynham.TicketGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.*;
+
 @SpringBootTest
 class TicketGeneratorApplicationTests {
 
